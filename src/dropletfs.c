23afe04be36002b174ced018dc72ae278b7bc8f6
poz <guillaume@bk.ru>
2010-10-12 01:05:59 +0200

    My very first commit

---
+++ src/dropletfs.c
@@ -0,0 +1,255 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <time.h>
+#include <sys/statvfs.h>
+
+#define FUSE_USE_VERSION 29
+#include <fuse.h>
+#include <droplet.h>
+
+dpl_ctx_t *ctx;
+FILE *fp;
+
+#define DPL_CHECK_ERR(func, rc, path) do {                              \
+        if (DPL_SUCCESS != rc) {                                        \
+                fprintf(fp, "%d - "#func " %s: %s\n",                   \
+                        (int)time(NULL), path, dpl_status_str(rc));     \
+                return -1;                                              \
+        }                                                               \
+} while(0)
+
+
+int
+dfs_getattr(const char *path,
+            struct stat *buf)
+{
+
+        dpl_dict_t *metadata = NULL;
+        dpl_status_t rc = dpl_getattr(ctx, (char *)path, &metadata);
+
+        if (DPL_SUCCESS != rc) {
+                fprintf(fp, "%d - dpl_getattr %s: %s\n",
+                        (int)time(NULL), path, dpl_status_str(rc));
+        }
+
+        if (metadata)
+                free(metadata);
+
+        return rc;
+}
+
+
+int
+dfs_mkdir(const char *path,
+          mode_t mode)
+{
+        dpl_status_t rc = dpl_mkdir(ctx, (char *)path);
+        DPL_CHECK_ERR(dpl_mkdir, rc, path);
+
+        return 0;
+}
+
+int
+dfs_rmdir(const char *path)
+{
+        dpl_status_t rc = dpl_rmdir(ctx, (char *)path);
+        DPL_CHECK_ERR(dpl_rmdir, rc, path);
+
+        return 0;
+}
+
+int
+dfs_unlink(const char *path)
+{
+        dpl_status_t rc = dpl_unlink(ctx, (char *)path);
+        DPL_CHECK_ERR(dpl_unlink, rc, path);
+
+        return 0;
+}
+
+int
+dfs_write(const char *path,
+          const char *buf,
+          size_t size,
+          off_t offset,
+          struct fuse_file_info *info)
+{
+        /* missing parameters */
+        dpl_status_t rc = dpl_write(NULL, (char *)buf, size);
+        DPL_CHECK_ERR(dpl_write, rc, path);
+
+        return 0;
+}
+
+int
+dfs_readdir(const char *path,
+            void *data,
+            fuse_fill_dir_t fill,
+            off_t offset,
+            struct fuse_file_info *info)
+{
+        void *dir_hdl;
+        dpl_dirent_t dirent;
+
+        dpl_status_t rc = dpl_chdir(ctx, (char *)path);
+        DPL_CHECK_ERR(dpl_chdir, rc, path);
+
+        rc = dpl_opendir(ctx, ".", &dir_hdl);
+        DPL_CHECK_ERR(dpl_opendir, rc, ".");
+
+        rc = dpl_readdir(dir_hdl, &dirent);
+        DPL_CHECK_ERR(dpl_readdir, rc, dirent.name);
+
+        dpl_closedir(dir_hdl);
+
+        return 0;
+}
+
+
+int
+dfs_opendir(const char *path,
+            struct fuse_file_info *info)
+{
+        void *dir_hdl;
+        dpl_status_t rc = dpl_opendir(ctx, (char *)path, &dir_hdl);
+        DPL_CHECK_ERR(dpl_opendir, rc, path);
+
+        return 0;
+}
+
+int
+dfs_statfs(const char *path, struct statvfs *buf)
+{
+        buf->f_flag = ST_RDONLY|8;
+        buf->f_namemax = 255;
+        buf->f_bsize = 4096;
+        buf->f_frsize = buf->f_bsize;
+        buf->f_blocks = buf->f_bfree = buf->f_bavail =
+                1000ULL * 1024 * 1024 * 1024 / buf->f_frsize;
+        buf->f_files = buf->f_ffree = 1000000000;
+
+        return 0;
+}
+
+int dfs_mknod(const char *path, mode_t mode) { return 0; }
+int dfs_readlink(const char *path, char *buf, size_t bufsiz) { return 0; }
+int dfs_symlink(const char *oldpath, const char *newpath) { return 0; }
+int dfs_rename(const char *oldpath, const char *newpath) { return 0; }
+int dfs_chmod(const char *path, mode_t mode) { return 0; }
+int dfs_chown(const char *path, uid_t uid, gid_t gid) { return 0; }
+int dfs_truncate(const char *path, off_t offset) { return 0; }
+int dfs_utime(const char *path, struct utimbuf *times) { return 0; }
+int dfs_open(const char *path, struct fuse_file_info *info) { return 0; }
+int dfs_flush(const char *path, struct fuse_file_info *info) { return 0; }
+int dfs_fsync(const char *path, int issync, struct fuse_file_info *info) { return 0; }
+int dfs_release(const char *path, struct fuse_file_info *info) { return 0; }
+int dfs_read(const char *path, char *buf, size_t siwe, off_t offset, struct fuse_file_info *info) { return 0; }
+
+
+struct fuse_operations dfs_ops = {
+        /* implemented */
+        .getattr    = dfs_getattr,
+        .mkdir      = dfs_mkdir,
+        .write      = dfs_write,
+        .readdir    = dfs_readdir,
+        .opendir    = dfs_opendir,
+        .unlink     = dfs_unlink,
+        .rmdir      = dfs_rmdir,
+
+        /* not implemented yet */
+        .readlink   = dfs_readlink,
+        .symlink    = dfs_symlink,
+        .rename     = dfs_rename,
+        .chmod      = dfs_chmod,
+        .chown      = dfs_chown,
+        .truncate   = dfs_truncate,
+        .utime      = dfs_utime,
+        .open       = dfs_open,
+        .flush      = dfs_flush,
+        .fsync      = dfs_fsync,
+        .release    = dfs_release,
+        .read       = dfs_read,
+        .statfs     = dfs_statfs,
+
+        .getdir     = NULL, /* deprecated */
+        .link       = NULL, /* no support needed */
+};
+
+
+static int dfs_fuse_main(struct fuse_args *args)
+{
+#if FUSE_VERSION >= 26
+        return fuse_main(args->argc, args->argv, &dfs_ops, NULL);
+#else
+        return fuse_main(args->argc, args->argv, &dfs_ops);
+#endif
+}
+
+
+static void droplet_pp(dpl_ctx_t *ctx)
+{
+#define PP(field, fmt) fprintf(stdout, #field": "fmt"\n", ctx->field)
+        PP(n_conn_buckets, "%d");
+        PP(n_conn_max, "%d");
+        PP(n_conn_max_hits, "%d");
+        PP(conn_idle_time , "%d");
+        PP(conn_timeout, "%d");
+        PP(read_timeout, "%d");
+        PP(write_timeout, "%d");
+        PP(use_https, "%d");
+        PP(host, "%s");
+        PP(port, "%d");
+
+        PP(access_key, "%s");
+        PP(secret_key, "%s");
+        PP(ssl_cert_file, "%s");
+        PP(ssl_key_file, "%s");
+        PP(ssl_password, "%s");
+        PP(ssl_ca_list, "%s");
+
+        PP(trace_level, "%d");
+
+        PP(pricing, "%s");
+        PP(read_buf_size, "%u");
+        PP(encrypt_key, "%s");
+        PP(delim, "%s");
+
+        PP(n_conn_fds, "%d");
+        PP(cur_bucket, "%s");
+        PP(droplet_dir, "%s");
+        PP(profile_name, "%s");
+#undef PP
+}
+
+
+int main(int argc, char **argv)
+{
+        struct fuse_args args = FUSE_ARGS_INIT(argc, argv);
+        int rc = EXIT_FAILURE;
+
+	dpl_status_t st = dpl_init();
+	if (DPL_SUCCESS != st) {
+		fprintf(fp, "dpl_init: %s\n", dpl_status_str(st));
+                goto err1;
+	}
+
+	ctx = dpl_ctx_new(NULL, NULL);
+	if (! ctx) {
+          goto err2;
+	}
+
+        droplet_pp(ctx);
+        fp = fopen("/tmp/fuse.log", "a");
+
+        rc = dfs_fuse_main(&args);
+        dpl_ctx_free(ctx);
+err2:
+	dpl_free();
+err1:
+        fclose(fp);
+
+	return rc;
+}
+
