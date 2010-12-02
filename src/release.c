c1bc364f201d4cdf1a206d49fbba61787ebfc98f
poz <guillaume@bk.ru>
2010-12-02 02:43:04 +0100

    since we do not handle cache, dfs_release() removes temp files

--- src/release.c
+++ src/release.c
@@ -4,11 +4,20 @@ int
 dfs_release(const char *path,
             struct fuse_file_info *info)
 {
-        int fd = ((struct pentry *)info->fh)->fd;
-        LOG("%s, fd=%d", path, fd);
+        struct pentry *pe = info->fh;
+        LOG("%s, fd=%d", path, pe->fd);
 
-        if (-1 != fd)
-                close(fd);
+
+        if (-1 != pe->fd)
+                close(pe->fd);
+
+        char local[128] = "";
+        snprintf(local, sizeof local, "/tmp/%s/%s",
+                 ctx->cur_bucket, path);
+        if (-1 == unlink(local))
+                LOG("unlink cache file(%s): %s", local);
+
+        g_hash_table_remove(hash, (char *)path);
 
         return 0;
 }
