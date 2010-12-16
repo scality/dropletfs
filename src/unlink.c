9c89eab14fd95b45f36cda43874ef5783442afc9
poz <guillaume@bk.ru>
2010-12-16 22:50:19 +0100

    move a piece of code from dfs_release() to dfs_unlink()

--- src/unlink.c
+++ src/unlink.c
@@ -8,5 +8,12 @@ dfs_unlink(const char *path)
         dpl_status_t rc = dpl_unlink(ctx, (char *)path);
         DPL_CHECK_ERR(dpl_unlink, rc, path);
 
+        char local[4096] = "";
+        snprintf(local, sizeof local, "/tmp/%s/%s", ctx->cur_bucket, path);
+        if (-1 == unlink(local))
+                LOG("unlink cache file(%s): %s", local, strerror(errno));
+
+        g_hash_table_remove(hash, (char *)path);
+
         return 0;
 }
