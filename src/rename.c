429b5fcdbaa4dd630697b378d87f5c16fba386af
poz <guillaume@bk.ru>
2011-01-11 01:34:14 +0100

    fix: dfs_rename() is ok now

--- src/rename.c
+++ src/rename.c
@@ -11,45 +11,25 @@ int
 dfs_rename(const char *oldpath,
            const char *newpath)
 {
+        dpl_status_t rc;
+        char *p = NULL;
+        int ret = 0;
+
         LOG("%s -> %s", oldpath, newpath);
 
-        struct get_data get_data = { .fd = -1, .buf = NULL };
-        dpl_vfile_t *vfile = NULL;
-        dpl_canned_acl_t canned_acl = DPL_CANNED_ACL_PRIVATE;
-        dpl_dict_t *dict = NULL;
-        dpl_status_t rc = DPL_FAILURE;
-        size_t size = 0;
-        char *str_size = NULL;
+        if (0 == strcmp(newpath, ".")) {
+                p = strrchr(oldpath, '/');
+                newpath = p ? p + 1 : oldpath;
+        }
 
-        rc = dpl_openread(ctx,
-                          (char *)oldpath,
-                          0u,
-                          NULL,
-                          cb_get_buffered,
-                          &get_data,
-                          &dict);
+        rc = dpl_fcopy(ctx, (char *)oldpath, (char *)newpath);
+        if (DPL_SUCCESS != rc) {
+                LOG("dpl_fcopy: %s", dpl_status_str(rc));
+                ret = -1;
+                goto err;
+        }
 
-        if (DPL_FAILURE == rc)
-                goto failure;
-
-        str_size = dpl_dict_get_value(dict, "size");
-        if (str_size)
-                size = (size_t)strtoul(str_size, NULL, 10);
-
-        rc = dpl_openwrite(ctx,
-                           (char *)newpath,
-                           DPL_VFILE_FLAG_CREAT|DPL_VFILE_FLAG_MD5,
-                           dict,
-                           canned_acl,
-                           size,
-                           &vfile);
-
-        if (DPL_FAILURE == rc)
-                goto failure;
-
-        return dfs_unlink(oldpath);;
-
-failure:
-        LOG("%s (%d)", dpl_status_str(rc), rc);
-        return -1;
+        ret = dfs_unlink(oldpath);
+  err:
+        return ret;
 }
