ea1092ad7093bc57f9d4b5e0bde63277b4dbc794
poz <guillaume@bk.ru>
2011-02-04 17:53:26 +0100

    fix: NULL pointer dereferencement

--- src/chown.c
+++ src/chown.c
@@ -4,6 +4,7 @@
 #include "log.h"
 #include "metadata.h"
 #include "timeout.h"
+#include "tmpstr.h"
 
 extern dpl_ctx_t *ctx;
 
@@ -15,33 +16,41 @@ dfs_chown(const char *path,
         dpl_dict_t *metadata = NULL;
         dpl_status_t rc;
         int ret;
+        char *buf = NULL;
 
-        LOG(LOG_DEBUG, "%s", path);
+        LOG(LOG_DEBUG, "%s, uid=%llu, gid=%llu",
+            path, (unsigned long long)uid, (unsigned long long)gid);
 
         rc = dfs_getattr_timeout(ctx, path, &metadata);
         if (DPL_SUCCESS != rc) {
-                LOG(LOG_ERR, "dfs_getattr_timeout: %s", dpl_status_str(rc));
+                LOG(LOG_ERR, "dpl_getattr: %s", dpl_status_str(rc));
                 ret = -1;
                 goto err;
         }
 
-        if (! metadata) {
-                LOG(LOG_NOTICE, "no metadata found");
+        if (! metadata)
+                metadata = dpl_dict_new(13);
+
+        buf = tmpstr_printf("%llu", (unsigned long long)uid);
+        if (DPL_SUCCESS != dpl_dict_update_value(metadata, "uid", buf)) {
                 ret = -1;
                 goto err;
         }
 
-        assign_meta_to_dict(metadata, "uid", &uid);
-        assign_meta_to_dict(metadata, "gid", &gid);
+        buf = tmpstr_printf("%llu", (unsigned long long)gid);
+        if (DPL_SUCCESS != dpl_dict_update_value(metadata, "gid", buf)) {
+                ret = -1;
+                goto err;
+        }
 
         rc = dfs_setattr_timeout(ctx, path, metadata);
-        if (DPL_FAILURE == rc) {
-                LOG(LOG_NOTICE, "dfs_setattr_timeout: %s", dpl_status_str(rc));
-                ret = rc;
+        if (DPL_SUCCESS != rc) {
+                LOG(LOG_ERR, "dpl_setattr: %s", dpl_status_str(rc));
+                ret = -1;
                 goto err;
         }
 
-        return 0;
+        ret = 0;
   err:
         if (metadata)
                 dpl_dict_free(metadata);
