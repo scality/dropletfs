543dc4305340b623a5666cdc96bf368fbd6e5b53
poz <guillaume@bk.ru>
2011-02-10 16:59:03 +0100

    enhancement: update entries of a directory, regardless to their last access time, if this directory was 'touched' recently.

--- src/cachedir.c
+++ src/cachedir.c
@@ -14,11 +14,69 @@
 #include "getattr.h"
 #include "timeout.h"
 #include "file.h"
+#include "list.h"
 
 #define MAX_CHILDREN 10
 
 extern struct conf *conf;
 extern dpl_ctx_t *ctx;
+extern GHashTable *hash;
+
+static void
+cb_map_dirents(void *elem, void *cb_arg)
+{
+        char *path = NULL;
+        dpl_dict_t *metadata = NULL;
+        dpl_status_t rc;
+        dpl_ftype_t type;
+        dpl_ino_t ino, parent_ino, obj_ino;
+        pentry_t *pe_dirent = NULL;
+        pentry_t *pe = NULL;
+
+        path = elem;
+        pe = cb_arg;
+
+        LOG(LOG_DEBUG, "path='%s', dirent='%s'", path, pentry_get_path(pe));
+
+        pe_dirent = g_hash_table_lookup(hash, path);
+        if (! pe_dirent) {
+                LOG(LOG_ERR, "'%s' is not an entry anymore in '%s'",
+                    path, pentry_get_path(pe));
+                goto end;
+        }
+
+        rc = dfs_namei_timeout(ctx, path, ctx->cur_bucket,
+                               ino, &parent_ino, &obj_ino, &type);
+
+        LOG(LOG_DEBUG, "path=%s, dpl_namei: %s, type=%s, parent_ino=%s, obj_ino=%s",
+            path, dpl_status_str(rc), ftype_to_str(type),
+            parent_ino.key, obj_ino.key);
+
+        if (DPL_SUCCESS != rc) {
+                LOG(LOG_NOTICE, "dfs_namei_timeout: %s", dpl_status_str(rc));
+                goto end;
+        }
+
+        rc = dfs_getattr_timeout(ctx, path, &metadata);
+        if (DPL_SUCCESS != rc && DPL_EISDIR != rc) {
+                LOG(LOG_ERR, "dfs_getattr_timeout: %s", dpl_status_str(rc));
+                goto end;
+        }
+
+        if (pentry_md_trylock(pe_dirent))
+                goto end;
+
+        if (metadata)
+                pentry_set_metadata(pe_dirent, metadata);
+
+        pentry_set_atime(pe_dirent);
+
+        (void)pentry_md_unlock(pe_dirent);
+  end:
+        if (metadata)
+                dpl_dict_free(metadata);
+
+}
 
 static void *
 update_metadata(void *cb_arg)
@@ -29,9 +87,12 @@ update_metadata(void *cb_arg)
         dpl_ino_t ino, parent_ino, obj_ino;
         dpl_status_t rc;
         dpl_dict_t *metadata = NULL;
+        struct list *dirent = NULL;
 
         pe = cb_arg;
         path = pentry_get_path(pe);
+
+        LOG(LOG_DEBUG, "path=%s", path);
 
         ino = dpl_cwd(ctx, ctx->cur_bucket);
 
@@ -53,6 +114,13 @@ update_metadata(void *cb_arg)
                 goto end;
         }
 
+        /* If this is a directory, update its entries' metadata */
+        if (DPL_FTYPE_DIR == type) {
+                dirent = pentry_get_dirents(pe);
+                if (dirent)
+                        list_map(dirent, cb_map_dirents, pe);
+        }
+
         if (pentry_md_trylock(pe))
                 goto end;
 
@@ -61,11 +129,10 @@ update_metadata(void *cb_arg)
 
         pentry_set_atime(pe);
 
+        (void)pentry_md_unlock(pe);
   end:
         if (metadata)
                 dpl_dict_free(metadata);
-
-        (void)pentry_md_unlock(pe);
 
         pthread_exit(NULL);
         return NULL;
